9512887cd862c264c9b81215b25063ae2c066bfb
ckbjimmy <ckbjimmy@gmail.com>
2017-01-14 00:56:18 -0500

    sarsa

--- rl_q_maze.py
+++ rl_q_maze.py
@@ -1,8 +1,10 @@
 from maze_env import Maze
 from rl_core import QLearningTable
+from rl_core import SarsaTable
+from rl_core import SarsaLambdaTable
         
 
-def update():
+def update_Q():
     for episode in range(100):
         # initial observation
         observation = env.reset()
@@ -32,9 +34,62 @@ def update():
     env.destroy()
 
 
+"""
+Sarsa is a online updating method for Reinforcement learning.
+
+Unlike Q learning which is a offline updating method, Sarsa is updating while in the current trajectory.
+
+You will see the sarsa is more coward when punishment is close because it cares about all behaviours,
+while q learning is more brave because it only cares about maximum behaviour.
+"""
+
+def update_Sarsa():
+    for episode in range(100):
+        # initial observation
+        observation = env.reset()
+
+        # RL choose action based on observation (different from Q)
+        action = RL.choose_action(str(observation))
+
+        while True:
+            # fresh env
+            env.render()
+
+            # RL take action and get next observation and reward
+            observation_, reward, done = env.step(action)
+
+            # RL choose action based on next observation
+            action_ = RL.choose_action(str(observation_))
+
+            # RL learn from this transition (s, a, r, s, a) ==> Sarsa
+            RL.learn(str(observation), action, reward, str(observation_), action_)
+
+            # swap observation and action
+            observation = observation_
+            action = action_
+
+            # break while loop when end of this episode
+            if done:
+                break
+
+    # end of game
+    print('game over')
+    env.destroy()
+
+
 if __name__ == "__main__":
     env = Maze()
     RL = QLearningTable(actions=list(range(env.n_actions)))
-
     env.after(100, update)
+    env.mainloop()    
+    
+    env = Maze()
+    RL = SarsaTable(actions=list(range(env.n_actions)))
+    env.after(100, update_Sarsa)
+    env.mainloop()
+    
+    
+    env = Maze()
+    RL = SarsaLambdaTable(actions=list(range(env.n_actions)))
+    env.after(100, update_Sarsa)
     env.mainloop()
